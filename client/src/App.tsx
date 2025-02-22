92a82c29399e36584bc07b1f609650eae268c9bf
Jari Haavisto <jari.haavisto@buutti.com>
2025-02-22 14:36:08 +0200

    add db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,82 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+type Value = {
+  id: number
+  value: string
+}
+
 function App() {
+  const [value, setValue] = useState('')
+  const [values, setValues] = useState<Array<Value>>([])
+
+  useEffect(() => {
+    const fetchValues = async () => {
+      const response = await fetch('/api/values')
+      if (!response.ok) {
+        console.error('Failed to fetch values')
+        return
+      }
+      const data = await response.json()
+      console.log(data)
+      setValues(data)
+    }
+    fetchValues()
+  }, [])
+
+  const submit = async () => {
+    const response = await fetch('/api/values', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({ value })
+    })
+    if (!response.ok) {
+      console.error('Failed to submit value')
+      return
+    }
+    const data = await response.json()
+    console.log('data', data)
+    setValues(values.concat(data))
+    setValue('')
+  }
+
+  const deleteValues = async () => {
+    const response = await fetch('/api/values', {
+      method: 'DELETE'
+    })
+    if (!response.ok) {
+      console.error('Failed to delete values')
+      return
+    }
+    setValues([])
+  }
+
   return (
     <>
       <h1>Aarnihauta</h1>
+      <div>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+        <button onClick={submit}>Submit</button>
+        <button onClick={deleteValues}>Delete</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {values.map((value, index) => (
+            <tr key={index}>
+              <td>{value.id}</td>
+              <td>{value.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,51 @@
 import express, { Request, Response } from 'express'
+import { Pool } from 'pg'
+
+const {
+  AARNIHAUTA_PORT: serverPortString,
+  AARNIHAUTA_PG_HOST: host,
+  AARNIHAUTA_PG_PORT: portString,
+  AARNIHAUTA_PG_USER: user,
+  AARNIHAUTA_PG_PASS: password,
+  AARNIHAUTA_PG_DB: database
+} = process.env;
+
+const port = Number(portString)
 
 const server = express()
+server.use(express.json())
 
 server.use(express.static('public'))
 
-server.get('/api', (req: Request, res: Response) => {
-  res.send('Hello world!')
+const pool = new Pool({ host, port, database, user, password })
+
+server.get('/api/values', async (req: Request, res: Response) => {
+  const queryText = 'SELECT * FROM values'
+  const client = await pool.connect()
+  const result = await client.query(queryText)
+  client.release()
+  res.send(result.rows)
 })
 
-server.listen(6001, () => {
-  console.log('Aarnihauta Server is running on http://localhost:6001')
+server.post('/api/values', async (req: Request, res: Response) => {
+  console.log(req.body)
+  const queryText = 'INSERT INTO values (value) VALUES ($1) RETURNING *'
+  const queryValues = [req.body.value]
+  console.log(queryValues)
+  const client = await pool.connect()
+  const result = await client.query(queryText, queryValues)
+  client.release()
+  res.send(result.rows)
+})
+
+server.delete('/api/values', async (req: Request, res: Response) => {
+  const queryText = 'DELETE FROM values'
+  const client = await pool.connect()
+  const result = await client.query(queryText)
+  client.release()
+  res.send()
+})
+
+server.listen(serverPortString, () => {
+  console.log(`Aarnihauta Server is running on http://localhost:${serverPortString}`)
 })
