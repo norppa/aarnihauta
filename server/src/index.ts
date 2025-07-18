a65270dc01caabd9962a069dc6ea7c7fb5b473bb
Jari Haavisto <jari.haavisto@buutti.com>
2025-07-18 16:58:15 +0300

    add root endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,10 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 
+server.get('/', (req: Request, res: Response) => {
+  res.send('Aarnihauta Server is running')
+})
+
 server.use('/api/store', storeRouter)
 
 const port = process.env.AARNIHAUTA_PORT
